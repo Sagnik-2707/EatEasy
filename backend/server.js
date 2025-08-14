fa8620b71c0c4199eee83859041e8f3e855823b6
Sagnik-2707 <sagnikmanna537@gmail.com>
2025-08-14 16:15:01 +0530

    Coded server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,19 @@
-const express = require('express');
-const PORT = 8000;
-const 
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import menuRoutes from "./routes/menu.js";
+import orderRoutes from "./routes/orders.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/menu", menuRoutes);
+app.use("/orders", orderRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env. PORT}`);
+});
