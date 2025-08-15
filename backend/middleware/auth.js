06b9b212f754a8a62494512fc5ec7c6a5808dcf8
Sagnik-2707 <sagnikmanna537@gmail.com>
2025-08-15 12:24:01 +0530

    Inital auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const adminAuth = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if(!token)
+        return res.status(401).json({message: "Unauthorized"});
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(decoded.role !== "admin")
+            return res.status(403).json({message : "Forbidden"});
+        next();
+    }
+    catch
+    {
+        res.status(401).json({message : "Invalid token"});
+    }
+};
