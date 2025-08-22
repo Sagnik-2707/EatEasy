ed1ec9fc67dcba76f9656e996f83818bda24bba2
Sagnik-2707 <sagnikmanna537@gmail.com>
2025-08-22 16:12:43 +0530

    Small changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import React, {useState} from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Banner from './components/Banner';
-import Navbar from './components/Navbar';
-import UserPage from './pages/UserPage';
-import AdminPage from './pages/AdminPage';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Banner from "./components/Banner";
+import UserPage from "./pages/UserPage";
+import AdminPage from "./pages/AdminPage";
+import "./App.css";
 
-    import '@fontsource/roboto/300.css';
-    import '@fontsource/roboto/400.css';
-    import '@fontsource/roboto/500.css';
-    import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import Dashboard  from "./pages/Dashboard";
 
 function App() {
   const [orders, setOrders] = useState([]);
@@ -19,12 +20,14 @@ function App() {
   return (
     <>
       <Banner />
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<UserPage  addOrder={addOrder} />} />
+        <Route path="/" element={<UserPage addOrder={addOrder} />} />
         <Route path="/admin" element={<AdminPage orders={orders} />} />
+        {/* <Route
+          path="/admin/dashboard"
+          element={<Dashboard />}
+        /> */}
       </Routes>
-      
     </>
   );
 }
