4520b59e2abbcc68b9a2736130ec6ae3492f590b
Sagnik-2707 <sagnikmanna537@gmail.com>
2025-09-07 01:00:56 +0530

    Placed OAuth button in frontend and styled it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -158,6 +158,19 @@ function Navbar() {
                 </>
               )}
             </p>
+            <div className="google-login">
+                <button
+                  onClick={() => {
+                    window.location.href = "http://localhost:5000/api/auth/google";
+                  }}
+                >
+                  <img
+                    src="https://developers.google.com/identity/images/g-logo.png"
+                    alt="Google Logo"
+                  />
+                  Continue with Google
+                </button>
+            </div>
           </div>
         </div>
       )}
